2127cdb740b19ad3d279dd17ffe721f01a2aa314
Bocksa <cian_mc_namara@hotmail.com>
2024-03-09 22:33:23 +0000

    basic req

--- basic_req/main.py
+++ basic_req/main.py
@@ -1,47 +1,55 @@
+ZForce = 0
+YForce = 0
+XForce = 0
 Force = 0
 AlarmType = 0
-
 input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
-datalogger.set_column_titles("force")
+datalogger.set_column_titles("X-Force", "Y-Force", "Z-Force", "AbsoluteForce")
 
 def on_microbit_id_io_p20_evt():
-    global AlarmType
     if AlarmType == 2:
         pins.digital_write_pin(DigitalPin.P2, 1)
+        music.play(music.tone_playable(988, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.LOOPING_IN_BACKGROUND)
         basic.show_leds("""
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
+            . # # # .
+            # # . # #
+            # . . . #
+            # # . # #
+            . # # # .
             """)
-    pins.digital_write_pin(DigitalPin.P1, 1)
-    basic.show_leds("""
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        """)
-    control.wait_micros(50)
-    AlarmType = 0
-    pins.digital_write_pin(DigitalPin.P1, 0)
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    control.on_event(EventBusSource.MICROBIT_ID_IO_P20, EventBusValue.MICROBIT_EVT_ANY, on_microbit_id_io_p20_evt)
+    if AlarmType == 1:
+        music.play(music.tone_playable(784, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.LOOPING_IN_BACKGROUND)
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+control.on_event(EventBusSource.MICROBIT_ID_IO_P20,
+    EventBusValue.MICROBIT_EVT_ANY,
+    on_microbit_id_io_p20_evt)
 
 def on_forever():
-    global Force, AlarmType
+    global Force, XForce, YForce, ZForce, AlarmType
+    Force = input.acceleration(Dimension.STRENGTH)
+    XForce = input.acceleration(Dimension.X)
+    YForce = input.acceleration(Dimension.Y)
+    ZForce = input.acceleration(Dimension.Z)
     if input.acceleration(Dimension.STRENGTH) >= 300:
-        Force = input.acceleration(Dimension.STRENGTH)
-        if Force >= 2000:
-            if Force >= 8000:
-                AlarmType = 2
-            else:
-                AlarmType = 1
-            control.raise_event(EventBusSource.MICROBIT_ID_IO_P20,
-                EventBusValue.MES_ALERT_EVT_ALARM1)
+        if Force >= 8000:
+            AlarmType = 2
         else:
-            AlarmType = 0
-        datalogger.log(datalogger.create_cv("force", Force))
-
+            if Force >= 3000:
+                AlarmType = 1
+            else:
+                AlarmType = 0
+        control.raise_event(EventBusSource.MICROBIT_ID_IO_P20,
+            EventBusValue.MES_ALERT_EVT_ALARM1)
+    datalogger.log(datalogger.create_cv("X-Force", XForce))
+    datalogger.log(datalogger.create_cv("Y-Force", YForce))
+    datalogger.log(datalogger.create_cv("Z-Force", ZForce))
+    datalogger.log(datalogger.create_cv("Y-Force", Force))
 basic.forever(on_forever)
