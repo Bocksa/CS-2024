d830d047837ccd2f20b0a0b1ecf59ed85962a8ac
Bocksa <cian_mc_namara@hotmail.com>
2024-03-14 19:27:35 +0000

    a lot happened

--- basic_req/main.py
+++ basic_req/main.py
@@ -1,11 +1,3 @@
-ZForce = 0
-YForce = 0
-XForce = 0
-Force = 0
-AlarmType = 0
-input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
-datalogger.set_column_titles("X-Force", "Y-Force", "Z-Force", "AbsoluteForce")
-
 def on_microbit_id_io_p20_evt():
     if AlarmType == 2:
         pins.digital_write_pin(DigitalPin.P2, 1)
@@ -21,6 +13,7 @@ def on_microbit_id_io_p20_evt():
     if AlarmType == 1:
         music.play(music.tone_playable(784, music.beat(BeatFraction.WHOLE)),
             music.PlaybackMode.LOOPING_IN_BACKGROUND)
+        pins.digital_write_pin(DigitalPin.P1, 1)
         basic.show_leds("""
             # # # # #
             # . . . #
@@ -32,6 +25,14 @@ control.on_event(EventBusSource.MICROBIT_ID_IO_P20,
     EventBusValue.MICROBIT_EVT_ANY,
     on_microbit_id_io_p20_evt)
 
+ZForce = 0
+YForce = 0
+XForce = 0
+Force = 0
+AlarmType = 0
+input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
+datalogger.set_column_titles("X-Force", "Y-Force", "Z-Force", "AbsoluteForce")
+
 def on_forever():
     global Force, XForce, YForce, ZForce, AlarmType
     Force = input.acceleration(Dimension.STRENGTH)
@@ -41,15 +42,15 @@ def on_forever():
     if input.acceleration(Dimension.STRENGTH) >= 300:
         if Force >= 8000:
             AlarmType = 2
+            control.raise_event(EventBusSource.MICROBIT_ID_IO_P20,
+                EventBusValue.MES_ALERT_EVT_ALARM1)
         else:
             if Force >= 3000:
                 AlarmType = 1
-            else:
-                AlarmType = 0
-        control.raise_event(EventBusSource.MICROBIT_ID_IO_P20,
-            EventBusValue.MES_ALERT_EVT_ALARM1)
-    datalogger.log(datalogger.create_cv("X-Force", XForce))
-    datalogger.log(datalogger.create_cv("Y-Force", YForce))
-    datalogger.log(datalogger.create_cv("Z-Force", ZForce))
-    datalogger.log(datalogger.create_cv("Y-Force", Force))
+                control.raise_event(EventBusSource.MICROBIT_ID_IO_P20,
+                    EventBusValue.MES_ALERT_EVT_ALARM1)
+    datalogger.log(datalogger.create_cv("X-Force", XForce),
+        datalogger.create_cv("Y-Force", YForce),
+        datalogger.create_cv("Z-Force", ZForce),
+        datalogger.create_cv("AbsoluteForce", Force))
 basic.forever(on_forever)
