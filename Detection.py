5206411894a611218ba3dd54c350a1ee4296ab7e
Bocksa <cian_mc_namara@hotmail.com>
2024-03-15 21:01:10 +0000

    unit testing

--- Detection.py
+++ Detection.py
@@ -1,8 +1,12 @@
 ﻿import math
+import sys
+
+args = sys.argv
 
 def Simulate(ImpactForce, Velocity, AngleOfImpact):
     Force = ImpactForce - 0.3
-    collisionChance = ((120 * Force) / Velocity) * math.fabs(math.cos((AngleOfImpact - 90) * math.pi / 720))
+
+    collisionChance = ((120 * Force) / Velocity) * math.fabs(math.cos(((AngleOfImpact - 90) * math.pi) / 720))
 
     if (collisionChance > 1):
         collisionChance = 1
@@ -10,4 +14,4 @@ def Simulate(ImpactForce, Velocity, AngleOfImpact):
         collisionChance = 0
     return collisionChance
     
-
+print(Simulate(args[0], args[1], args[2]))
